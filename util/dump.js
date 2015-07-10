242526099bce34cea773b02f79ffa6f2b4074846
David Oliveira <d.oliveira@prozone.org>
2015-07-10 23:13:07 +0100

    Updated dump util

--- util/dump.js
+++ util/dump.js
@@ -1,32 +1,61 @@
 var
 	fs		= require('fs'),
-	header	= new Buffer(5),
-	fd;
+	uuid		= require('uuid'),
+	header		= new Buffer("OIIIIIIIIIIIIIIIISSSST"),
+	fd,
+	file = process.argv[2] || 'queue.db';
+
+// Decode the header of an entry
+function _entryDecodeHeader(b) {
+
+	var
+		item = {};
+
+	// Operation
+	item.op = b[0];
+
+	// Convert the binary ID into a string ID
+	try {
+		item.id = uuid.unparse(b,1);
+	}
+	catch(ex){
+		console.log("Unable to parse ID");
+		item.id = null;
+	}
+
+	// Data size
+	item.size = (b[17] << 24) | (b[18] << 16) | (b[19] << 8) | b[20];
+
+	// Type
+	item.type = b[21];
+
+	return item;
+
+}
 
 // Open and read all data inside of the file
-fd = fs.openSync("file_async.db","r");
-while ( fs.readSync(fd,header,0,5) ) {
+fd = fs.openSync(file,"r");
+while ( fs.readSync(fd,header,0,header.length) ) {
 	var
-		op   = (header[0] >> 4) & 0x0f,
-		type = header[0] & 0x0f,
-		size = (header[1] << 24) | (header[2] << 16) | (header[3] << 8) | header[4],
-		data = new Buffer(size),
+		entry = _entryDecodeHeader(header),
+		data = new Buffer(entry.size),
 		rBytes;
 
 	// Read data
-	if ( size > 0 && (rBytes = fs.readSync(fd,data,0,size)) < size ) {
-		console.log("Queue file appears to be corrupted (header size is "+size+" and got "+rBytes+")");
+	if ( entry.size > 0 && (rBytes = fs.readSync(fd,data,0,entry.size)) < entry.size ) {
+		console.log("Queue file appears to be corrupted (header size is "+entry.size+" and got "+rBytes+")");
 		break;
 	}
 
-	console.log("OP: ",op);
-	console.log("TY: ",type);
-	console.log("SI: ",size);
+	console.log("OP: ",entry.op);
+	console.log("ID: ",entry.id);
+	console.log("SI: ",entry.size);
+	console.log("TY: ",entry.type);
 
 	// Parse
-	if ( size > 0 ) {
-		data = (type == 1) ? data.toString() : (type == 2) ? parseFloat(data.toString()) : (type == 3) ? null : JSON.parse(data);
-		console.log("DT: ",data," ("+size+" bytes)");
+	if ( entry.size > 0 ) {
+		data = (entry.type == 1) ? data.toString() : (entry.type == 2) ? parseFloat(data.toString()) : (entry.type == 3) ? null : JSON.parse(data);
+		console.log("DT: ",data," ("+entry.size+" bytes)");
 	}
 }
 fs.close(fd);
